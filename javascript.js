ca18a641d392c7c762718fa7c3abf16c88a2697b
madhu231105 <madhumithra2311@gmail.com>
2025-01-24 12:25:22 +0530

    message

---
+++ javascript.js
@@ -0,0 +1,12 @@
+let user={
+    name:"ABC"
+,    age:30,
+    "is singer":true,
+}
+for(let key in user){
+    console.log(key+":"user[key]);
+}
+const array=[1,2,3,6,5,4]
+for(let value of array){
+    console.log(value);
+}
